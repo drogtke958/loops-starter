bef71cb2f94db0643e772619ce2b66bcd14fa2f4
drogtke958 <153301526+drogtke958@users.noreply.github.com>
2024-11-06 10:31:43 -0500

    Create range.py

---
+++ range.py
@@ -0,0 +1,42 @@
+# Keily Drogt
+# Nov 06, 2024
+# Range Function Practice
+
+# 4-3
+nums = list(range(1, 20))
+for n in nums:
+  print(n)
+
+# 4-4
+onemil = range(1, 1000000000)
+for n in onemil:
+  print(n)
+
+mil = range(1, 1000000000)
+min(mil)
+max(mil)
+sum(mil)
+for i in mil:
+  print(i)
+
+# 4-6
+odd_num = range(1, 12, 1)
+for x in odd_num:
+  print(x)
+
+# 4-7
+multiple_three = range(3, 30)
+for x in multiple_three:
+  print(x)
+
+# 4-8
+cubes = [value**2 for value in range(1.10)]
+for c in cubes:
+  print(c)
+
+
+
+
+
+
+
